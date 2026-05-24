7a9ae2f13bafc7fece6d198888de194e0c862e19
Eduardo Rodrigues <eduardoanjosrodrigues@gmail.com>
2026-05-24 15:02:47 -0300

    correcao beste production

--- app/controllers/pwa_controller.rb
+++ app/controllers/pwa_controller.rb
@@ -1,5 +1,5 @@
 class PwaController < ApplicationController
-  skip_before_action :authenticate_user!
+  skip_before_action :authenticate_user!, raise: false
   skip_before_action :check_onboarding, raise: false
 
   def offline
